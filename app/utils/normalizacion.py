514482a30191cc50cc08135972305940ddfde0f4
Antonio Sánchez Buzón <amtoniosazon@gmail.com>
2026-03-20 10:33:45 +0100

    Add additional key variations for normalization

--- app/utils/normalizacion.py
+++ app/utils/normalizacion.py
@@ -9,16 +9,16 @@ def normalizar_campos_linea(linea: dict) -> dict:
         if key in ["pedido", "pedidoid", "numero", "nº pedido"]:
             nueva["NumeroPedido"] = v
 
-        elif key in ["cliente", "customer"]:
+        elif key in ["cliente", "customer", "Cliente", "Client"]:
             nueva["Cliente"] = v
 
-        elif key in ["fecha", "fecha salida", "fecha_salida", "fcarga"]:
+        elif key in ["fecha", "fecha salida", "fecha_salida", "fcarga", "fecha de carga"]:
             nueva["FechaSalida"] = v
 
-        elif key in ["mercancia", "producto", "articulo"]:
+        elif key in ["mercancia", "producto", "articulo", "mercancía"]:
             nueva["Mercancia"] = v
 
-        elif key in ["origen", "carga", "punto carga", "pcarga"]:
+        elif key in ["origen", "carga", "punto carga", "pcarga", "punto de carga"]:
             nueva["PuntoCarga"] = v
 
         elif key in ["confeccion", "confección"]:
@@ -26,7 +26,13 @@ def normalizar_campos_linea(linea: dict) -> dict:
 
         elif key in ["categoria", "cat", "cat.", "ct"]:
             nueva["Categoria"] = v
-
+            
+        elif key in ["calibre", "cal", "cal."]:
+            nueva["Calibre"] = v
+            
+        elif key in ["comercial", "comer.", "Comercial"]:
+            nueva["comercial"] = v
+            
         else:
             nueva[k] = v
 
